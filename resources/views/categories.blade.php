7e424c180a98fb48325ef5fcdcd4e822f474adaa
Caroluce Ricky Harkris N <105916769+CaroluceRickyHarkrisN@users.noreply.github.com>
2022-11-29 03:47:13 +0700

    Edit posts, Update api image

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -21,7 +21,7 @@
             <div class="col-md-4 mb-3">
                 <a href="/posts?category={{ $category->slug }}">
                     <div class="card bg-dark text-white">
-                        <img src="https://source.unsplash.com/500x500?{{ $category->name }}" class="card-img" alt="{{ $category->name }}">
+                        <img src="https://picsum.photos/500/s500?{{ $category->name }}" class="card-img" alt="{{ $category->name }}">
                         <div class="card-img-overlay d-flex align-items-center p-0">
                         <h3 class="card-title text-center flex-fill p-4" style="background-color:rgba(0, 0, 0, 0.7)">{{ $category->name }}</h3>
                         </div>
