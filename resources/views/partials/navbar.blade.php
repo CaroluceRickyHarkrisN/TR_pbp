520d53a152c414e9c8def3d59a01b39406fcae6a
usmansyachh <usmansyach123@gmail.com>
2022-11-29 10:32:29 +0700

    js usman

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -107,3 +107,15 @@
     </div>
   </div>
 </nav>
+
+<script>
+  const navbar = document.getElementsByTagName('nav')[0]
+  window.addEventListener('scroll', function() {
+    console.log(window.scrollY);
+    if (window.scrollY > 1) {
+      navbar.classList.replace('bg-transparent', 'nav-color');
+    } else if (this.window.scrollY <= 0) {
+      navbar.classList.replace('nav-color', 'bg-transparent')
+    }
+  });
+</script>
