2fa3ad8e8d077592fc898f31e6bdb23ac6371ace
michael <michaelrio89@gmail.com>
2022-11-30 14:38:14 +0700

    update login dan register

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -19,37 +19,45 @@
             @if (session()->has('success'))
                 <div class="alert alert-success alert-dismissible fade show" role="alert">
                     {{ session('success') }}
-                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                    <a type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></a>
                 </div>
             @endif
 
             @if (session()->has('loginError'))
                 <div class="alert alert-danger alert-dismissible fade show" role="alert">
                     {{ session('loginError') }}
-                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                    <a type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></a>
                 </div>
             @endif
+            <br>
+            <br>
+            <main class="loginbox">
+                <br>
+                <img src="/img/riologreg/avatar2.png" class="avatar">
+                <br>
+                <br>
+                <br>
+                <br>
 
-            <main class="form-signin">
-                <h1 class="h3 mb-4 fw-normal text-center">Login Form</h1>
 
                 <form action="/login" method="post">
                     @csrf
-                    <div class="form-floating">
-                        <input type="email" name="email" class="form-control @error('email') is-invalid @enderror" id="email"  placeholder="name@example.com" autofocus required value="{{ old('email') }}">
-                        <label for="email">Email address</label>
+                    <div class="input">
+                        <title for="email">Sign in to Onotodewe</title>
+                        <input type="email" name="email" class="form-control @error('email') is-invalid @enderror" id="email"  placeholder="Email address" autofocus required value="{{ old('email') }}">
                         @error('email')
                             <div class="invalid-feedback">
                                 {{ $message }}
                             </div>
+                    </div>
                         @enderror
+
+                    <div class="input">
+                        <input type="password" name="password" class="form-control" id="password" placeholder="Password" required>
+
                     </div>
 
-                    <div class="form-floating">
-                        <input type="password" name="password" class="form-control"  id="password" placeholder="Password" required>
-                        <label for="password">Password</label>
-                    </div>
-                    <button class="w-100 btn btn-lg btn-dark" type="submit">Login</button>
+                    <button class="submit" type="submit">Login</button>
                 </form>
 
                 <small class="d-block text-center mt-3">Not registered? &nbsp;<a href="/register">Register</a></small>
--- resources/views/register/index.blade.php
+++ resources/views/register/index.blade.php
@@ -10,18 +10,24 @@
     <link rel="stylesheet" href="rio/css/style.css">
 </head>
 <body>
-
     @include('partials.navbar')
     <br><br><br><br><br>
     <div class="row justify-content-center">
         <div class="col-lg-5 mt-3">
-            <main class="form-registration">
-                <h1 class="h3 mb-4 fw-normal text-center">Registration Form</h1>
+
+            <main class="registerbox">
+                <img src="/img/riologreg/library.png" class="avatar2">
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <title for="email">Please Register here</title>
                 <form action="/register" method="post">
                     @csrf
-                    <div class="form-floating">
+                    <div class="input">
                         <input type="text" name="name" class="form-control rounded-top @error('name') is-invalid @enderror" id="name" placeholder="Name" required value="{{ old('name') }}">
-                        <label for="name">Name</label>
                         @error('name')
                             <div class="invalid-feedback">
                                 {{ $message }}
@@ -29,9 +35,8 @@
                         @enderror
                     </div>
 
-                    <div class="form-floating">
-                        <input type="text" name="username" class="form-control @error('username') is-invalid @enderror" id="username" placeholder="Username" required value="{{ old('username') }}">
-                        <label for="username">Username</label>
+                    <div class="input">
+                        <input type="text" name="username" class="form-control @error('username') is-invalid @enderror" id="username" placeholder="Username" autofocus required value="{{ old('username') }}">
                         @error('username')
                             <div class="invalid-feedback">
                                 {{ $message }}
@@ -39,9 +44,8 @@
                         @enderror
                     </div>
 
-                    <div class="form-floating">
-                        <input type="email" name="email" class="form-control @error('email') is-invalid @enderror" id="email" placeholder="name@example.com" required value="{{ old('email') }}">
-                        <label for="email">Email address</label>
+                    <div class="input">
+                        <input type="email" name="email" class="form-control @error('email') is-invalid @enderror" id="email" placeholder="Email@example.com" required value="{{ old('email') }}">
                         @error('email')
                             <div class="invalid-feedback">
                                 {{ $message }}
@@ -49,9 +53,8 @@
                         @enderror
                     </div>
 
-                    <div class="form-floating">
+                    <div class="input">
                         <input type="password" name="password" class="form-control rounded-bottom @error('password') is-invalid @enderror" id="password" placeholder="Password" required>
-                        <label for="password">Password</label>
                         @error('password')
                             <div class="invalid-feedback">
                                 {{ $message }}
@@ -59,14 +62,13 @@
                         @enderror
                     </div>
 
-                    <button class="w-100 btn btn-lg btn-dark mt-3" type="submit">Register</button>
+                    <button class="submit" type="submit">Register</button>
                 </form>
-
                 <small class="d-block text-center mt-3">Already registered? &nbsp;<a href="/login">Login</a></small>
             </main>
         </div>
     </div>
-    <br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+    <br><br><br><br>
 
     @include('partials.footer')
 
