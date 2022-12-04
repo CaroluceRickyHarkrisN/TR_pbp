a44eff3968bae27970bf8cc936095bd50bc17342
Caroluce Ricky Harkris N <105916769+CaroluceRickyHarkrisN@users.noreply.github.com>
2022-12-04 17:34:52 +0700

    Admin_Super_Fix

---
+++ resources/views/dashboard/adminposts/index.blade.php
@@ -0,0 +1,47 @@
+@extends('dashboard.layouts.main')
+
+@section('container')
+    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <h1 class="h2">All Books</h1>
+    </div>
+
+    @if (session()->has('success'))
+        <div class="alert alert-success col-lg-10" role="alert">
+            {{ session('success') }}
+        </div>
+    @endif
+
+    <div class="table-responsive col-lg-10">
+        <a href="/dashboard/posts/create" class="btn btn-primary mb-3 rounded-pill"> Add New Book</a>
+        <table class="table">
+          <thead class="thead-dark">
+            <tr>
+              <th scope="col">No</th>
+              <th scope="col">Title</th>
+              <th scope="col">Category</th>
+              <th scope="col">Setting</th>
+            </tr>
+          </thead>
+          <tbody>
+            @foreach ($posts as $post)
+            <tr>
+              <td>{{ $loop->iteration }}</td>
+              <td>{{ $post->title }}</td>
+              <td>{{ $post->category->name }}</td>
+              <td>
+                <a href="/dashboard/posts/{{ $post->slug }}" class="badge bg-dark rounded-pill"><span data-feather="eye"></span></a>
+                <a href="/dashboard/posts/{{ $post->slug }}/edit" class="badge bg-warning rounded-pill"><span data-feather="settings"></span></a>
+                <form action="/dashboard/posts/{{ $post->slug }}" method="post" class="d-inline">
+                    @method('delete')
+                    @csrf
+                    <button class="badge bg-danger border-0 rounded-pill" onclick="return confirm('Delete ?')"><span data-feather="trash-2"></span></button>
+                </form>
+              </td>
+            </tr>
+
+            @endforeach
+
+          </tbody>
+        </table>
+      </div>
+@endsection
