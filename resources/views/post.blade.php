2e71b76e03a1a2d37b3d81f356c149c6808c0837
Caroluce Ricky Harkris N <105916769+CaroluceRickyHarkrisN@users.noreply.github.com>
2022-12-05 17:39:29 +0700

    AdminV2

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -6,7 +6,7 @@
         <div class="row justify-content-center mb-5">
             <div class="col-md-8">
                 <h1 class="mb-3">{{ $post->title }}</h1>
-                <p><small class="text-muted">Author : <a href="/posts?author={{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> | Category : <a href="/posts?category={{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></small></p>
+                <p><small class="text-muted">Author : {{ $post->author->name }}</a> | Category : <a href="/posts?category={{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></small></p>
 
 
                 @if ($post->image)
